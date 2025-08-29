5c18cd81aa60b51c14a13654632066df4e98dbdc
kahlidfma5 <kahlidfma5@hotmail.com>
2025-08-29 17:21:15 +0000

    2-then.js

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,4 +1,6 @@
-/* then => (success function, error function) */
 export default function handleResponseFromAPI(promise) {
-  return promise.then(() => ({ status: 200, body: 'success' })).catch(() => Error()).finally(() => console.warn('Got a response from the API'));
+  return promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
 }
