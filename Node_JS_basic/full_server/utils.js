e74b82037e6342398a44ff1c090d555411e09679
kahlidfma5 <147338247+kahlidfma5@users.noreply.github.com>
2025-09-07 19:37:32 +0300

    Update utils.js

--- Node_JS_basic/full_server/utils.js
+++ Node_JS_basic/full_server/utils.js
@@ -1,42 +1,34 @@
 const fs = require('fs');
 
-module.exports = function readDatabase(path) {
+function readDatabase(path) {
   return new Promise((resolve, reject) => {
-    fs.readFile(path, { encoding: 'utf-8' }, (err, data) => {
-      if (err) return reject(Error('Cannot load the database'));
-      // split data and taking only list without header
-      const lines = data.split('\n').slice(1, -1);
-      // give the header of data
-      const header = data.split('\n').slice(0, 1)[0].split(',');
-      // find firstname and field index
-      const idxFn = header.findIndex((ele) => ele === 'firstname');
-      const idxFd = header.findIndex((ele) => ele === 'field');
-      // declarate two dictionaries for count each fields and store list of students
-      const fields = {};
-      const students = {};
-      // it will contain all data
-      const all = {};
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(Error(err));
+        return;
+      }
+      const content = data.toString().split('\n');
 
-      lines.forEach((line) => {
-        const list = line.split(',');
-        if (!fields[list[idxFd]]) fields[list[idxFd]] = 0;
-        fields[list[idxFd]] += 1;
-        if (!students[list[idxFd]]) students[list[idxFd]] = '';
-        students[list[idxFd]] += students[list[idxFd]]
-          ? `, ${list[idxFn]}`
-          : list[idxFn];
-      });
-      for (const key in fields) {
-        if (Object.hasOwnProperty.call(fields, key)) {
-          const number = fields[key];
-          all[key] = {
-            students: `List: ${students[key]}`,
-            number,
-          };
+      let students = content.filter((item) => item);
+
+      students = students.map((item) => item.split(','));
+
+      const fields = {};
+      for (const i in students) {
+        if (i !== 0) {
+          if (!fields[students[i][3]]) fields[students[i][3]] = [];
+
+          fields[students[i][3]].push(students[i][0]);
         }
       }
 
-      return resolve(all);
+      delete fields.field;
+
+      resolve(fields);
+
+      //   return fields;
     });
   });
-};
+}
+
+export default readDatabase;
